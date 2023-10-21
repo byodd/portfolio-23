5fe20103df9dee31a61be0f5eb8ded1d7d098cf1
p2201085 <gatta.code@gmail.com>
2023-10-21 16:19:50 +0200

    404 page added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import Bot from "../../public/errorbot.png"
+import Header from './components/header'
+
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <main className="flex bg-soft-grey text-dark-grey min-h-screen flex-col box-border">
+      <Header/>
+      <div className='flex flex-col my-auto items-center content-center'>
+        <Image src={Bot} className='w-52' alt='sadbot'></Image>
+        <span className='font-bold text-5xl'>404</span>
+        <p className='font-semibold text-3xl'>Page not found. Go back to <Link className='text-soft-blue font-bold' href="/">Home</Link></p>
+      </div>
+    </main>
+  )
+}
